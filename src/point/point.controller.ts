821a5bd4afa3dee8d2994b04ea1f2257cd214a82
wooo73 <ywaves073@gmail.com>
2024-12-18 14:32:53 +0900

    fix: 유저 포인트 조회 PointService 적용
    - point controller 파일 내 미사용 객체 정리
    - TODO 작성

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -1,17 +1,14 @@
 import { Body, Controller, Get, Param, Patch, ValidationPipe } from '@nestjs/common';
-import { PointHistory, UserPoint } from './point.model';
-import { UserPointTable } from 'src/database/userpoint.table';
-import { PointHistoryTable } from 'src/database/pointhistory.table';
-import { PointBody as PointDto } from './point.dto';
+
 import { PointService } from './point.service';
+
+import { PointHistory, UserPoint } from './point.model';
+
+import { PointBody as PointDto } from './point.dto';
 
 @Controller('/point')
 export class PointController {
-    constructor(
-        private readonly userDb: UserPointTable,
-        private readonly historyDb: PointHistoryTable,
-        private readonly pointService: PointService,
-    ) {}
+    constructor(private readonly pointService: PointService) {}
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -21,7 +18,7 @@ export class PointController {
     @Get(':id')
     async point(@Param('id') id): Promise<UserPoint> {
         const userId = Number.parseInt(id);
-        return await this.userDb.selectById(userId);
+        return await this.pointService.selectUserPoint(userId);
     }
 
     /**
@@ -40,6 +37,7 @@ export class PointController {
      * * 컨트롤러 실행시
      * - 충전 금액 검증 -> 충전 포인트가 0 이하 또는 숫자가 아닐 경우 && 충전 결과값 10_000_000 이상일 경우 실패
      * - 유저 조회 -> 존재하지 않는 유저일 경우 실패
+     *      -> TODO: 유저가 존재하지 않는 조건은 제외
      * - 유저 포인트 충전 -> TODO: 먼저 요청으로 넘어온 유저에 대해 응답 후 다음 로직 실행(순서 보장) -> 테스트 후 결과 확인 후 수정 예정
      *      - 이력 생성
      *      - 증가된 유저 포인트 응답
@@ -56,6 +54,7 @@ export class PointController {
      *
      * - 사용할 포인트 검증 -> 사용 포인트 0 이하 또는 숫자가 아닐 경우 && 사용 포인트가 보유 포인트 초과시 실패
      * - 유저 조회 -> 존재하지 않는 유저일 경우 실패
+     *       -> TODO: 유저가 존재하지 않는 조건은 제외
      * - 유저 포인트 차감 -> TODO: 먼저 요청으로 넘어온 유저에 대해 응답 후 다음 로직 실행(순서 보장) -> 테스트 후 결과 확인 후 수정 예정
      *      - 이력 생성
      *      - 사용된 유저 포인트 응답
