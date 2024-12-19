d60799339e4062ece458a2130c0e14afac619443
wooo73 <ywaves073@gmail.com>
2024-12-19 23:25:24 +0900

    fix: 최대 보유 포인트 멤버 변수 설정

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -9,12 +9,12 @@ import { TransactionType } from '../point/point.model';
  * 첫 진입 -> 유저 요청 -> Map내 해당 유저 ID 존재 여부 확인 -> Mutex 인스턴스 생성 -> Mutex 인스턴스 잠금 활성 후 비즈니스 로직 실행 -> 잠금 해제 후 Map내 해당 유저 ID 삭제
  *
  * 1. 같은 유저 동시 요청
- * Map내 해당 유저 ID 존재 여부 확인 -> 기존 Mutex 잠금 여부 확인 -> 잠긴경우 대기 / 열린경우 잠금 활성 후 비즈니스 로직 실행 -> ''
- * -> 같은 유저일 경우 동일한 Mutex 인스턴스(같은 길)을 공유 하기때문에 순차적 처리 가능
- * -> 충전 / 사용과 같은 다른 메서드에서도 동일하게 적용되므로 순차적 처리 가능
+ *    Map내 해당 유저 ID 존재 여부 확인 -> 기존 Mutex 잠금 여부 확인 -> 잠긴경우 대기 / 열린경우 잠금 활성 후 비즈니스 로직 실행 -> ''
+ *      -> 같은 유저일 경우 동일한 Mutex 인스턴스(같은 길)을 공유 하기때문에 순차적 처리 가능
+ *      -> 충전 / 사용과 같은 다른 메서드에서도 동일하게 적용되므로 순차적 처리 가능
  *
  * 2. 다른 유저 동시 요청
- * -> 다른 유저일 경우 신규 Mutex 인스턴스(새로운 길)을 이용하기 때문에 병렬 처리 가능
+ *      -> 다른 유저일 경우 신규 Mutex 인스턴스(새로운 길)을 이용하기 때문에 병렬 처리 가능
  *
  */
 
@@ -24,6 +24,8 @@ export class PointService {
         private readonly userDb: UserPointTable,
         private readonly historyDb: PointHistoryTable,
     ) {}
+
+    private readonly LimitChargePoint = 10_000_000;
 
     async chargePoint(userId: number, amount: number) {
         try {
@@ -38,8 +40,7 @@ export class PointService {
 
             const chargePoint = amount + userSelect.point;
 
-            //TODO: 최대 보유값은 객체 멤버변수로 관리 할 것.
-            if (chargePoint >= 10_000_000) {
+            if (chargePoint >= this.LimitChargePoint) {
                 throw new BadRequestException('보유 금액을 초과했습니다.');
             }
 
